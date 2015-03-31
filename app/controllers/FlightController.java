d290d9c5dff7f16c844df898ea65c282e88ca40a
Nicolas <wicowyn@gmail.com>
2015-03-31 15:24:07 +0200

    Add CRUD API to flights

--- app/controllers/FlightController.java
+++ app/controllers/FlightController.java
@@ -1,15 +1,18 @@
 package controllers;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
 import entity.Flight;
-import play.Logger;
+import entity.Hotel;
+import entity.User;
 import play.db.jpa.JPA;
 import play.db.jpa.Transactional;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
-import play.mvc.Security;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -18,26 +21,8 @@ import java.util.List;
  */
 public class FlightController extends Controller {
 
-    public static final String AUTH_KEY="X-Session-Token";
-
-    @Transactional
-    public static Result createFlight() {
-        JsonNode jsonNode=request().body().asJson();
-        FlightCreationInput input=Json.fromJson(jsonNode, FlightCreationInput.class);
-
-        Logger.debug("Arrival: " + input.arrival + " | Departure: " + input.departure);
-
-        // Create flight entity
-        Flight flight=new Flight( input.departure, input.arrival, input.dateDeparture, input.dateArrival);
-
-        // Store it in DB
-        JPA.em().persist(flight);
-
-        return ok(Json.toJson(flight));
-    }
-
     @Transactional(readOnly = true)
-    public static Result getFlight(int id) {
+    public static Result get(int id) {
         Flight flight= JPA.em().find(Flight.class, id);
 
         if(flight==null) {
@@ -48,19 +33,72 @@ public class FlightController extends Controller {
         }
     }
 
+    @Transactional
+    public static Result delete(int id) {
+        Flight flight= JPA.em().find(Flight.class, id);
+
+        if(flight!=null) {
+            JPA.em().remove(flight);
+
+            return ok();
+        }
+        else {
+            return noContent();
+        }
+    }
+
+    @Transactional
+    public static Result create() {
+        JsonNode jsonNode=request().body().asJson();
+        FlightInput input=Json.fromJson(jsonNode, FlightInput.class);
+
+        Flight flight=new Flight();
+
+        flight.setArrival(input.end);
+        flight.setDateArrival(input.endDate);
+        flight.setDateDeparture(input.startDate);
+        flight.setDeparture(input.start);
+
+        ArrayList<Hotel> hotels=new ArrayList<>(input.hotels.length);
+
+        for(int id : input.hotels) {
+            Hotel hotel=JPA.em().find(Hotel.class, id);
+
+            if(hotel!=null) hotels.add(hotel);
+        }
+
+        if(hotels.size()==0) {
+            return badRequest("You need at least one hotel");
+        }
+
+        if(input.endDate.before(input.startDate)) {
+            return badRequest("end date can't be after start date");
+        }
+
+        flight.setHotels(hotels);
+
+        JPA.em().persist(flight);
+
+        return ok(Json.toJson(flight.getId()));
+    }
+
     @Transactional(readOnly = true)
-    @Security.Authenticated(Secured.class)
-    public static Result getAllFlight() {
-        List<Flight> list=JPA.em().createQuery("FROM Flight").getResultList();
-//        List<Flight> list=JPA.em().createNamedQuery("Flight.findAll").getResultList();
+//    @Security.Authenticated(Secured.class)
+    public static Result getAll() {
+        List<User> list=JPA.em().createQuery("FROM Flight").getResultList();
 
         return ok(Json.toJson(list));
     }
 
-    public static class FlightCreationInput {
-        public String departure;
-        public String arrival;
-        public Date dateDeparture;
-        public Date dateArrival;
+    public static class FlightInput {
+        public String start;
+        public String end;
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyy")
+        @JsonProperty("start_date")
+        public Date startDate;
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyy")
+        @JsonProperty("end_date")
+        public Date endDate;
+        public int[] hotels;
     }
 }
