0309b7d2443207fe1c78f7651d35f92e5f03733e
Bumbadawg <maitre.orbe@gmail.com>
2015-04-01 18:15:32 +0200

    / ----> redirige vers la page de login mise en comment de la page welcome du play framework parce que le bootstrap il est trop méga joli d'abord

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,17 +1,13 @@
 package controllers;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import play.Logger;
-import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
-import play.mvc.With;
 import views.html.*;
 
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready. KIKOO"));
+        return ok(main.render("FlyService - The service full of flies", login_template.render()));
     }
 
     public static Result angular() {
@@ -36,29 +32,5 @@ public class Application extends Controller {
 
     public static Result routing() {
         return ok(routing.render());
-    }
-
-
-    @With(MyAction.class)
-    @FirstAnnotation
-    public static Result plop() {
-        Logger.info("Call plop");
-        Lol lol=new Lol();
-        lol.setAge(54);
-
-        return ok(Json.toJson(lol));
-    }
-
-    public static class Lol {
-        private int age;
-
-        @JsonProperty("yolo")
-        public int getAge() {
-            return age;
-        }
-
-        public void setAge(int age) {
-            this.age = age;
-        }
     }
 }
